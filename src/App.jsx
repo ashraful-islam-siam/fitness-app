0cd38d6c3e00174c922649798ca4a7fd2c10b697
ashraful-islam-siam <ashrafulislam.dev1@gmail.com>
2022-10-03 15:07:05 +0600

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import Navbar from './components/Navbar'
-const App = () => {
-  return (
-    <div>
-      <Navbar />
-    </div>
-  )
-}
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-export default App
+import './App.css';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+const App = () => (
+  <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+    </Routes>
+    <Footer />
+  </Box>
+);
+
+export default App;
